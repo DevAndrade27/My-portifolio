57f74bafef3f15c88782d52e100b858e86a6a236
DevAndrade27 <vinicinsantos43@gmail.com>
2025-06-15 16:09:45 -0300

    feat: alterações feita pelo pc, criação do theme, estilização do Typography

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <CssBaseline />
-    {/* CssBaseline provides a consistent baseline for styles across browsers */}
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {/* CssBaseline provides a consistent baseline for styles across browsers */}
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
